bd45765584a6219740f8d58b664bdda5e1a6969c
Andrew0Hill <andrew.2.hill@ucdenver.edu>
2019-07-09 13:49:20 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="ipprl_tools",
+      version="1.0",
+	  description="A collection of tools to assist with performing IPPRL Linkage",
+	  author="Andrew Hill",
+	  author_email="andrew.2.hill@ucdenver.edu",
+	  install_requires=[
+		"numpy",
+		"pandas",
+		"fuzzy"],
+	  packages=["ipprl_tools"])
