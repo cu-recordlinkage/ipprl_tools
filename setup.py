5c14045a708c25cb7ece4376133f22d2ce588940
Andrew Hill <andrew.2.hill@ucdenver.edu>
2019-08-26 16:09:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="ipprl_tools",
-      version="1.0.1",
+      version="1.0",
 	  description="A collection of tools to assist with performing IPPRL Linkage",
 	  author="Andrew Hill",
 	  author_email="andrew.2.hill@ucdenver.edu",
@@ -9,5 +9,7 @@ setup(name="ipprl_tools",
 		"numpy",
 		"scipy",
 		"pandas"],
-	  packages=["ipprl_tools"],
-          include_package_data=True)
+	  packages=["ipprl_tools","ipprl_tools.utils"],
+          package_data={"ipprl_tools":["docs/*"]},
+          include_package_data=True
+     )
